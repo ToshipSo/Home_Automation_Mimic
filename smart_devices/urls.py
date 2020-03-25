dc1a418d78e002debd3e7a340c4a81c1f64c35d8
toship <sonkusare14@gmail.com>
2020-03-25 17:22:57 +0530

    Added home page view.

--- smart_devices/urls.py
+++ smart_devices/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from smart_devices.views import *
 
 
 urlpatterns = [
-
+    path('', Home.as_view(), name='home'),
 ]
--- smart_devices/views.py
+++ smart_devices/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import View, ListView
+from smart_devices.models import *
+
 
 # Create your views here.
+class Home(ListView):
+    queryset = Room.objects.all()
+    template_name = 'home.html'
+    context_object_name = 'rooms'
